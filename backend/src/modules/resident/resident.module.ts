c607ab1f390cc32895a7c2f7a254a598024a087e
HOA Project <hoa-system@project.local>
2026-04-01 22:00:05 +0800

    fix: add AuditModule to ResidentModule

--- backend/src/modules/resident/resident.module.ts
+++ backend/src/modules/resident/resident.module.ts
@@ -3,9 +3,13 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { ResidentService } from './resident.service';
 import { ResidentController } from './resident.controller';
 import { ResidentProfileEntity } from './entities/resident-profile.entity';
+import { AuditModule } from '../../common/audit/audit.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([ResidentProfileEntity])],
+  imports: [
+    TypeOrmModule.forFeature([ResidentProfileEntity]),
+    AuditModule,
+  ],
   controllers: [ResidentController],
   providers: [ResidentService],
   exports: [ResidentService],
